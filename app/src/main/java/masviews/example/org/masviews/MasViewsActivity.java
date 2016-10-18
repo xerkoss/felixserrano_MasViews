222b7fab4b892c14f98ce4a7e2c6cab5039b7d0a
xerkoss <el-xuko-1989@hotmail.com>
2016-10-18 03:22:27 +0200

    Ejercicio 2.10

--- app/src/main/java/masviews/example/org/masviews/MasViewsActivity.java
+++ app/src/main/java/masviews/example/org/masviews/MasViewsActivity.java
@@ -3,17 +3,26 @@ package masviews.example.org.masviews;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
+import android.widget.EditText;
 
 public class MasViewsActivity extends AppCompatActivity {
+
+    private EditText entrada;
+    private EditText salida;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mas_views);
+        entrada = (EditText)findViewById(R.id.entrada);
+        salida = (EditText)findViewById(R.id.salida);
     }
 
     public void sePulsa(View view){
-        Toast.makeText(this,"Pulsado",Toast.LENGTH_SHORT).show();
+        salida.setText(String.valueOf(Float.parseFloat(entrada.getText().toString().isEmpty() ? "0" : entrada.getText().toString()) *2.0));
+    }
+
+    public void sePulsa0(View view){
+        entrada.setText(entrada.getText() + (String)view.getTag());
     }
 }
